75ff44b0eccfb02433d9a2672a22d0f3a6291a2c
Joshua Tolhuis <jotolhuis@bioinf.nl>
2021-10-13 13:51:38 +0200

    seting up .idea project and gradle builder

---
+++ WekaApiRunner/src/main/java/WekaApiRunner.java
@@ -0,0 +1,11 @@
+import weka.classifiers.trees.J48;
+import weka.core.Instance;
+import weka.core.Instances;
+
+import java.io.IOException;
+
+public class WekaApiRunner {
+    public static void main(String[] args) {
+
+    }
+}
