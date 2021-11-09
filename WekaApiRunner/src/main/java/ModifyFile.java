8ca161f346a4c75ae1adc6b2300d1efb20495285
Joshua Tolhuis <jotolhuis@bioinf.nl>
2021-11-09 14:35:02 +0100

    implemented CLI (unfinished), updatet R script, can't test it yet because CLI doesn't work

--- WekaApiRunner/src/main/java/ModifyFile.java
+++ WekaApiRunner/src/main/java/ModifyFile.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class ModifyFile {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Class to alter the given script by a user using r
+ *
+ * @author Joshua Tolhuis
+ */
+
+public class ModifyFile {
+    protected void modifyData(String unknownFile) throws IOException, InterruptedException {
+        System.out.println(unknownFile);
+        ProcessBuilder pb = new ProcessBuilder("Rscript", "R_script.R", unknownFile);
+        Process p = pb.start();
+
+        TimeUnit.SECONDS.sleep(5);
+
+        System.out.println("DONE");
+    }
 }
--- WekaApiRunner/src/main/java/WekaApiRunner.java
+++ WekaApiRunner/src/main/java/WekaApiRunner.java
@@ -1,6 +1,8 @@
+import org.apache.commons.cli.*;
+
 import weka.classifiers.functions.Logistic;
-import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.converters.ConverterUtils;
 import weka.core.converters.ConverterUtils.DataSource;
 
 import java.io.FileNotFoundException;
@@ -8,24 +10,46 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
+
+/**
+ * Class with Apache CLI configured. This class will use a file given by a user and
+ * classify the stage of cancer according to a machine learning algorithm "Logistic"
+ *
+ * @author Joshua Tolhuis
+ */
+
 public class WekaApiRunner {
-    private final String modelFile = "src/main/resources/Logistics.model";
+    String[] arguments;
+    private static final String HELP = "help";
+    private static final String FILE = "file-name";
+
+    private Options options;
+    /**
+     * Main function passing command line arguments to the start function
+     *
+     * @param args: The command line arguments
+     */
 
     public static void main(String[] args) {
-        WekaApiRunner runner = new WekaApiRunner();
-        runner.start();
+        WekaApiRunner wekaApiRunner = new WekaApiRunner();
+        wekaApiRunner.start(args);
     }
-
-    private void start() {
-        String data = "weka_data.arff";
-        String unknownFile = "weka_data.arff";
+    public void start(String[] args){
+        this.arguments = args;
+        buildOptions();
+        String filename = parseArguments();
 
         try {
-            Instances instances = loadArff(data);
-            printInstances(instances);
-            Logistic log = buildClassifier(instances);
+            // Modify files using an R script
+            ModifyFile modifyFile = new ModifyFile();
+            modifyFile.modifyData(filename);
+            // Load instances
+            Instances instances = loadData();
+            // Load the classifier
             Logistic classifier = loadClassifier();
+            // Classify the instances with the model
             Instances classification = classifyNewInstance(classifier, instances);
+            // Write the classified instances to a .csv file
             writeToFile(classification);
 
         } catch (Exception e) {
@@ -33,12 +57,60 @@ public class WekaApiRunner {
         }
     }
 
-    private Instances classifyNewInstance(Logistic log, Instances unknownInstances) throws Exception {
+    /**
+     * Configuring the Apache CLI options for the command line.
+     */
+    public void buildOptions() {
+        this.options = new Options();
+
+        // Creating the arguments possible
+
+        Option help = new Option("h", "help", false, "Help function" );
+        Option fileName = new Option("f", "file-name", true, "The file name to classify from." );
+
+        options.addOption(help);
+        options.addOption(fileName);
+    }
+
+    /**
+     * Function to parse the command line arguments given by the user.
+     *
+     * @return filename/null: If the user gave a filename it will return the filename, if not null
+     *                        will be returned and an error will be thrown.
+     */
+    public String parseArguments() {
+        // Parsing input from the command line
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter helpFormatter = new HelpFormatter();
+
+        try {
+            CommandLine command = parser.parse(this.options, this.arguments);
+            // Check if the user entered the HELP argument
+            if (command.hasOption(HELP)){
+                helpFormatter.printHelp("Pancreatic Cancer Predictor", this.options, true);
+                return null;
+            } else if (command.hasOption(FILE)){
+                String fileName = command.getOptionValue(FILE);
+                return fileName;
+            }
+        } catch (ParseException e) {
+            // Printing the help when something goes wrong
+            helpFormatter.printHelp("Pancreatic Cancer Predictor", this.options, true);
+        }
+        return null;
+    }
+    /**
+     * Method to classify new instances using the abstract classifier.
+     *
+     * @param log: The loaded weka clasifier
+     * @param instances: The instances from the file the user entered
+     */
+    private static Instances classifyNewInstance(Logistic log, Instances instances) throws Exception {
         // create copy
-        Instances labeled = new Instances(unknownInstances);
+        Instances labeled = new Instances(instances);
         // label instances
-        for (int i = 0; i < unknownInstances.numInstances(); i++) {
-            double clsLabel = log.classifyInstance(unknownInstances.instance(i));
+        for (int i = 0; i < instances.numInstances(); i++) {
+            double clsLabel = log.classifyInstance(instances.instance(i));
             System.out.println(labeled.instance(i));
             labeled.instance(i).setClassValue(clsLabel);
         }
@@ -46,54 +118,41 @@ public class WekaApiRunner {
         return labeled;
     }
 
-
-    private void printInstances(Instances instances) {
-        int numAttributes = instances.numAttributes();
-
-        for (int i = 0; i < numAttributes; i++) {
-            System.out.println("attribute " + i + " = " + instances.attribute(i));
-        }
-
-        System.out.println("class index = " + instances.classIndex());
-
-        int numInstances = instances.numInstances();
-        for (int i = 0; i < numInstances; i++) {
-            if (i == 5) break;
-            Instance instance = instances.instance(i);
-            System.out.println("instance = " + instance);
-        }
-    }
-
-    private Logistic loadClassifier() throws Exception {
-        // deserialize model
+    /**
+     * Method to load the Logistics classifier .model file.
+     *
+     * @return Logistic object made from the .model Logistics file.
+     */
+    private static Logistic loadClassifier() throws Exception {
+        // Load in the Logistics model
+        String modelFile = "Logistics.model";
         return (Logistic) weka.core.SerializationHelper.read(modelFile);
     }
 
-    private Logistic buildClassifier(Instances instances) throws Exception {
-        String[] options = new String[1];
-        options[0] = "-R 1.0E-8";
-        Logistic log = new Logistic();
-        printInstances(instances);
-        return log;
-    }
-
-
-    private Instances loadArff(String dataset) throws IOException {
+    /**
+     * Method that loads the unknown data
+     *
+     * @return the data file
+     */
+    private Instances loadData() throws IOException {
         try {
-            DataSource source = new DataSource(dataset);
+            ConverterUtils.DataSource source = new ConverterUtils.DataSource("unknowndata/data.arff");
             Instances data = source.getDataSet();
-            // setting class attribute if the data format does not provide this information
-            // For example, the XRFF format saves the class attribute information as well
             if (data.classIndex() == -1)
                 data.setClassIndex(data.numAttributes() - 1);
             return data;
         } catch (Exception e) {
-            throw new IOException("could not read from file");
+            throw new IOException("Something went wrong while reading file. Please check if the" +
+                    "file is the right format.");
         }
     }
 
 
-    private void writeToFile(Instances classification) throws FileNotFoundException, UnsupportedEncodingException {
+    /**
+     * Function to write the new classification to a file
+     * @param classification: The classification
+     */
+    private static void writeToFile(Instances classification) throws FileNotFoundException, UnsupportedEncodingException {
         PrintWriter writer = new PrintWriter("classification.csv", "UTF-8");
         writer.println(classification);
         writer.close();
